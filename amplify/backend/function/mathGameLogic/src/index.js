336dc73081d935e827faeecabf1c78eb1517e498
David Amebley <davameb@gmail.com>
2024-01-01 05:39:07 +0200

    Configure file permissions for project directory

--- amplify/backend/function/mathGameLogic/src/index.js
+++ amplify/backend/function/mathGameLogic/src/index.js
@@ -7,11 +7,11 @@ exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
     return {
         statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
+        //  Uncomment below to enable CORS requests
+        //  headers: {
+        //      "Access-Control-Allow-Origin": "*",
+        //      "Access-Control-Allow-Headers": "*"
+        //  },
+        body: JSON.stringify('Hello from Lambd'),
     };
 };
