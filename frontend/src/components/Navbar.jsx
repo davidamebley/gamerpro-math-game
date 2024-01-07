d555188577fd219b7f668e0cdfb07a5de869e5f9
David Amebley <davameb@gmail.com>
2024-01-07 02:20:53 +0200

    Create a basic Navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/game">Game</Link></li>
+                <li><Link to="/leaderboard">Leaderboard</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
