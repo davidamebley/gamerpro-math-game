9040301fc964af49e7d83bd5ebf2ea070b90cdf3
David Amebley <davameb@gmail.com>
2024-01-07 02:28:28 +0200

    Create new AppRouter.js file with react-router-dom's BrowserRouter warpping Navbar, Home, GameInterface, and Leaderboard pages

---
+++ frontend/src/AppRouter.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage/HomePage";
+import GameInterface from "./pages/GameInterface/GameInterface";
+import Leaderboard from "./pages/Leaderboard/Leaderboard";
+
+const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/game" element={<GameInterface />} />
+                <Route path="/leaderboard" element={<Leaderboard />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default AppRouter;
