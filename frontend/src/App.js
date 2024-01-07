92782afa5a7c2d2149aefd84f9f00b18a39f1b39
David Amebley <davameb@gmail.com>
2024-01-07 12:52:22 +0200

    Add pattern class to the body of the App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
+
 import AppRouter from './AppRouter';
 import './App.css';
+import './styles/patterns.css'
 
 function App() {
+  useEffect(() => {
+    // Add pattern class to the body
+    document.body.classList.add('weave');
+  }, []);
+
   return (
     <div className="App">
       <AppRouter />
