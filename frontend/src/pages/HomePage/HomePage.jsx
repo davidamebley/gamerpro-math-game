d910a3ad9dd024024ce45e5c8e6ce2aaae93daea
David Amebley <davameb@gmail.com>
2024-01-08 05:06:57 +0200

    Import new Navbar.css file into HomePage.css

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './HomePage.css';
+import '../../components/Navbar/Navbar.css';
 
 const HomePage = () => {
     return (
