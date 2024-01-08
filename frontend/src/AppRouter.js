453c8166e14d6813bf1a2b3ce1e84cb0535724f3
David Amebley <davameb@gmail.com>
2024-01-08 05:10:25 +0200

    Change the path of imported Navbar component in AppRouter.jsx

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./pages/HomePage/HomePage";
 import GameInterface from "./pages/GameInterface/GameInterface";
 import Leaderboard from "./pages/Leaderboard/Leaderboard";
