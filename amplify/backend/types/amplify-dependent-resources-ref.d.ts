f58af96e9af7eaa9b0a5d4ddb15d74fbe8441798
David Amebley <davameb@gmail.com>
2024-01-11 08:56:11 +0200

    Set up and configure Cognito authentication with Amplify:
    - Initialize Cognito User Pool with email as the primary sign-in method.
    - Configure user sign-up attributes including email, unique username, first name, and last name.
    - Disable OAuth, Lambda Triggers, and additional Cognito capabilities for streamlined authentication.
    - Remove unused Identity Pool following reconfiguration.
    - Deploy changes with Amplify.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -10,8 +10,6 @@ export type AmplifyDependentResourcesAttributes = {
     "gamerpromathgame": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
