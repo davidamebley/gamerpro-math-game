640fe8b517c499b80d291ffe830a15c1f4a7cb42
David Amebley <davameb@gmail.com>
2024-01-12 09:23:05 +0200

    Initialize AWS Amplify configuration in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
