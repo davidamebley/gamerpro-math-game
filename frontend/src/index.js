f9990f66dd60caf1a620a7a8ba79077d150586f1
David Amebley <davameb@gmail.com>
2024-01-13 06:00:11 +0200

    Configure AWS Amplify with project-specific settings

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Amplify from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
 
 import './index.css';
