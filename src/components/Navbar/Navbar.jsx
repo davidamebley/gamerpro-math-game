1a861bb51d1ae66105b4b1249528e0e571f4e5f6
David Amebley <davameb@gmail.com>
2024-01-15 05:19:43 +0200

    Update Navbar component to fetch user info from aws-amplify on mount

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,37 @@
 import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getCurrentUser, fetchUserAttributes } from 'aws-amplify/auth';
 
 import './Navbar.css';
 
 const Navbar = () => {
+    const [userInfo, setUserInfo] = useState(null);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const getCurrentUserInfo = async () => {
+        const { username } = await getCurrentUser();
+        const attributes = await fetchUserAttributes();
+        return { username, attributes };
+    };
+
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                const data = await getCurrentUserInfo();
+                setUserInfo(data.attributes.preferred_username);
+            } catch (error) {
+                console.log('Error fetching user info: ', error);
+            }
+        };
+
+        fetchUserInfo();
+    }, []);
     return (
         <nav>
             <ul>
-                <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
-                <li><NavLink to="/game" activeClassName="active">Game</NavLink></li>
-                <li><NavLink to="/leaderboard" activeClassName="active">Leaderboard</NavLink></li>
+                <li><NavLink exact="true" to="/" activeclassame="active">Home</NavLink></li>
+                <li><NavLink to="/game" activeclassname="active">Game</NavLink></li>
+                <li><NavLink to="/leaderboard" activeclassname="active">Leaderboard</NavLink></li>
             </ul>
         </nav>
     );
