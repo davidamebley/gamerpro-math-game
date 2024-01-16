c1abefa6bd28214cc093b4036d5174010835728d
David Amebley <davameb@gmail.com>
2024-01-16 06:55:04 +0200

    Import FontAwsome package into project

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { getCurrentUser, fetchUserAttributes, signOut } from 'aws-amplify/auth';
 
 import defaultAvatar from '../../assets/default-avatar.png';
